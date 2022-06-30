738e53c2d64a984bd44f7933f30ad5c29e0dac3c
medmssaouri <mohamed.mssaouri@kitware.com>
2022-06-30 13:39:27 +0200

    Add reading images from bin file

--- demo.py
+++ demo.py
@@ -17,9 +17,16 @@ from utils.utils import InputPadder
 
 DEVICE = 'cuda'
 
-def load_image(imfile):
-    img = np.array(Image.open(imfile)).astype(np.uint8)
+#img = np.array(Image.open(imfile)).astype(np.uint8)
+
+def load_image(file_path, image_nb_elements, idx, image_size, image_dims):
+    img = np.reshape(np.fromfile(file_path, 'float32', image_nb_elements, '', idx * image_size), image_dims)
+    img *= 255
+    img = np.transpose(img, (1, 0, 2))
+    img = img.astype(np.uint8)
+    img_ = img
     img = torch.from_numpy(img).permute(2, 0, 1).float()
+    cv2.imwrite("/home/mohamed/Desktop/data/result_raft/1.png", img_)
     return img[None].to(DEVICE)
 
 
@@ -35,11 +42,19 @@ def viz(img, flo):
     # plt.imshow(img_flo / 255.0)
     # plt.show()
 
-    cv2.imshow('image', img_flo[:, :, [2,1,0]]/255.0)
+    cv2.imwrite("/home/mohamed/Desktop/data/result_raft/2.png", img_flo[:, :, [2,1,0]]/255.0)
+    cv2.imshow('image', img_flo[:, :, ]/255.0)
     cv2.waitKey()
 
 
 def demo(args):
+
+    dims = [640, 512, 3]
+    img_number_elements = dims[0]*dims[1]*dims[2]
+    number_frames = 20
+    FLOAT_SIZE = 4
+
+
     model = torch.nn.DataParallel(RAFT(args))
     model.load_state_dict(torch.load(args.model))
 
@@ -51,10 +66,11 @@ def demo(args):
         images = glob.glob(os.path.join(args.path, '*.png')) + \
                  glob.glob(os.path.join(args.path, '*.jpg'))
         
+        images_idxs = [i for i in range(number_frames)]
         images = sorted(images)
-        for imfile1, imfile2 in zip(images[:-1], images[1:]):
-            image1 = load_image(imfile1)
-            image2 = load_image(imfile2)
+        for img_idx_1, img_idx_2 in zip(images_idxs[:-1], images_idxs[1:]):
+            image1 = load_image(args.path, img_number_elements, img_idx_1, img_number_elements*FLOAT_SIZE, dims)
+            image2 = load_image(args.path, img_number_elements, img_idx_2, img_number_elements*FLOAT_SIZE, dims)
 
             padder = InputPadder(image1.shape)
             image1, image2 = padder.pad(image1, image2)
